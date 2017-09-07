6dc48ca05c645c1f991ae4d8b35d0c2f7079cd8c
pavel-sherepo <gebrial@icloud.com>
2017-09-07 23:00:15 +0300

    initialisation for tiny filler-module

---
+++ djmodel_filler/__init__.py
@@ -0,0 +1,13 @@
+from deep_mapper import process_mapping
+
+
+class ModelFiller(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    def transfer_data(self, data, map_options, map_root_path):
+        mapped_data = process_mapping(
+            data, map_options, map_root_path) if map_options else data
+        for item in mapped_data:
+            self.model.objects.create(**item)
